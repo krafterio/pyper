c0c9d3ee25191a203e5cab558fac8c80ece34928
François Pluchino <francois.pluchino@gmail.com>
2023-10-12 22:59:25 +0200

    [ADD] Add pyper_saas addon

---
+++ pyper_saas/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper SaaS',
+    'category': 'Hidden',
+    'license': 'Other proprietary',
+    'description': 'Configuration for SaaS',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'data/bot.xml',
+        'data/rules.xml',
+    ],
+    'assets': {
+        'web.assets_backend': {
+            'pyper_saas/static/src/webclient/**/*.js',
+        },
+    },
+}
