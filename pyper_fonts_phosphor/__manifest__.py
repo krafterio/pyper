fffccf71a612c9eabb45d9d2bc08d8f2b790b7ac
François Pluchino <francois.pluchino@gmail.com>
2023-10-12 22:59:51 +0200

    [ADD] Add pyper_fonts_phosphor addon

---
+++ pyper_fonts_phosphor/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper Fonts Phosphor',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'Phosphor fonts.',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'depends': [
+        'base_setup',
+        'web',
+    ],
+}
