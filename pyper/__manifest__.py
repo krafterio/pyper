348937f07ccc78762172c15f3e98b8ad883cb1bf
François Pluchino <francois@krafter.io>
2023-11-04 15:47:24 +0100

    [IMP] Reorder web asset primary variables in manifest

--- pyper/__manifest__.py
+++ pyper/__manifest__.py
@@ -22,8 +22,8 @@
     ],
     'assets': {
         'web._assets_primary_variables': [
-            ('before', 'web/static/src/**/*.variables.scss', 'pyper/static/src/**/*.variables.scss'),
             ('before', 'web/static/src/scss/primary_variables.scss', 'pyper/static/src/scss/primary_variables.scss'),
+            ('before', 'web/static/src/**/*.variables.scss', 'pyper/static/src/**/*.variables.scss'),
         ],
         'web._assets_secondary_variables': [
             ('before', 'web/static/src/scss/secondary_variables.scss', 'pyper/static/src/scss/secondary_variables.scss'),
