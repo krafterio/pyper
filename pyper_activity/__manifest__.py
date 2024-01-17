4df2fa90c87b64e4d505de13ebf0603d2c866bf0
Fabien Maquin <fabien@krafter.io>
2024-01-17 19:33:21 +0100

    [ADD] Add pyper activity addon

---
+++ pyper_activity/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper Activity',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'Pyper addons dedicated to activity override.',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'depends': [
+        'pyper',
+        'mail',
+    ],
+    'data': [
+    ],
+    'assets': {
+    },
+}
