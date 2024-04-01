86be9d9b28c5a7db288d60e1c584e8a4855ce6ac
François Pluchino <francois@krafter.io>
2024-04-01 11:28:58 +0200

    [ADD] Add dedicated app section for Pyper addons in Settings application

---
+++ pyper_setup/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper Setup',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'Add dedicated app section for Pyper addons in Settings application.',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'depends': [
+        'base',
+        'base_setup',
+        'web',
+    ],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+}
