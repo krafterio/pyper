90f428e8318a8c2c22580e4eba049d19d9d38dc9
François Pluchino <francois@krafter.io>
2024-04-24 19:08:12 +0200

    [ADD] Add pyper_portal_saas addon

---
+++ pyper_portal_saas/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper Portal SaaS',
+    'category': 'Hidden',
+    'license': 'Other proprietary',
+    'description': 'Portal configuration for SaaS',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'auto_install': [
+        'pyper_saas',
+        'web',
+        'portal',
+    ],
+    'depends': [
+        'pyper_saas',
+        'web',
+        'portal',
+    ],
+    'data': [
+        'views/portal_templates.xml',
+    ],
+}
