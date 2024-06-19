b05f3dda8ffdee4e6625f5b4da769dc3b2d4922f
Fabien Maquin <fabien@krafter.io>
2024-06-19 10:34:36 +0200

    [ADD] Add pyper product condition price

---
+++ pyper_product_condition_price/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Odoo Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper product condition price',
+    'category': 'Manufacturing/Repair',
+    'license': 'Other proprietary',
+    'description': 'This addon adds ability to manage product condition custom prices.',
+    'version': '17.0.0.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'application': True,
+    'depends': [
+        'product',
+        'pyper_product_condition',
+    ],
+    'data': [
+        # Security
+        'security/product_security.xml',
+
+        # Views
+        'views/conf_setting_views.xml',
+        'views/product_product_views.xml',
+        'views/product_template_views.xml',
+    ],
+}
