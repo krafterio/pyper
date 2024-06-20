967fc270acd14c0b48b6ffab3890501a9e063529
Fabien Maquin <fabien@krafter.io>
2024-06-20 06:52:58 +0200

    [ADD] Add pyper product extend features Book addon

---
+++ pyper_product_extend_features_book/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Odoo Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper product extend features book',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'This addon adds book product management.',
+    'version': '17.0.0.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'application': True,
+    'depends': [
+        'product',
+        'pyper_product_catalog',
+        'pyper_product_extend_features',
+    ],
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
+
+        # Views
+        'views/product_category_views.xml',
+        'views/product_template_views.xml',
+        'views/product_book_publisher_views.xml',
+        'views/menu.xml',
+    ],
+}
---
+++ pyper_product_extend_features_book/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Odoo Proprietary License (see LICENSE file).
+
+from . import product_category
+from . import book_publisher
+from . import product_template
