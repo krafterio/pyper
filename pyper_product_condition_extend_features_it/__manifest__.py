ffdc1132a5daf8b925b3eed02be11e6fdc65db74
Fabien Maquin <fabien@krafter.io>
2024-07-04 12:51:10 +0200

    [ADD] Improve catalog management

---
+++ pyper_product_condition_extend_features_it/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Odoo Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper product condition extend features IT',
+    'category': 'Manufacturing/Repair',
+    'license': 'Other proprietary',
+    'description': 'This addon adds ability to display condition on dedicated views with extends logics.',
+    'version': '17.0.0.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'application': True,
+    'auto_install': [
+        'pyper_product_condition',
+        'pyper_product_extend_features_it',
+    ],
+    'depends': [
+        'pyper_product_condition',
+        'pyper_product_extend_features_it',
+    ],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+}
