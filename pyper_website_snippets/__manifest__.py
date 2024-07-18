215d4ff3eb6f1a936494c62eecb7695fd2090fc6
Fabien Maquin <fabien@krafter.io>
2024-07-18 06:55:16 +0200

    [ADD] Add pyper website snippets addon

---
+++ pyper_website_snippets/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Pyper Website Snippets',
+    'category': 'Hidden',
+    'license': 'Other proprietary',
+    'description': 'Extend website snippets',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/snippets.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            '/pyper_website_snippets/static/src/scss/style.scss',
+        ],
+    }
+}
