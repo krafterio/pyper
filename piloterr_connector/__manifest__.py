712fdf6ce3cb57b25d968479b0e9d614de2cc6e3
Maude Vivier <maude@krafter.io>
2024-08-21 15:50:17 +0200

    [ADD] Add the piloterr_connector addon to connect and make a request with Piloterr, and Dooki sheet to explain how to use this addon #PYP81

---
+++ piloterr_connector/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Piloterr connector',
+    'category': 'Sales/CRM',
+    'license': 'Other proprietary',
+    'description': 'Add ability to get info from piloterr',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'application': True,
+    'installable': True,
+    'depends': [
+        'pyper',
+        'pyper_queue_job',
+    ],
+    'data': [
+        # Views
+        'views/res_config_settings_views.xml',
+    ],
+}
---
+++ piloterr_connector/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+from . import res_config_settings
+from . import piloterr_mixin
