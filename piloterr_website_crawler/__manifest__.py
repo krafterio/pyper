2b9912d98dbd6052e16b30ad7c53afbe897b309f
Maude Vivier <maude@krafter.io>
2024-08-23 11:40:45 +0200

    [ADD] Add addon to retrieve html from web page #PYP87

---
+++ piloterr_website_crawler/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Piloterr website crawler',
+    'category': 'Sales/CRM',
+    'license': 'Other proprietary',
+    'description': 'Add ability to get the HMTL of a web page from piloterr',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'application': True,
+    'installable': True,
+    'depends': [
+        'piloterr_connector',
+    ],
+    'data': [
+        # Views
+        'views/res_partner_views.xml',
+
+        # Security
+        'security/ir.model.access.csv',
+    ],
+}
---
+++ piloterr_website_crawler/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+from . import piloterr_website_crawler
+from . import res_partner
+from . import crawler_resultat
