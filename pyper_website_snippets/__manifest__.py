b830e10e43f54bcb3e40829ce16724fb1c37353e
Loup Martineau <loup@krafter.io>
2024-09-09 17:03:28 +0200

    [RFC] reorganize snippets files (part2) #PYP143

--- pyper_website_snippets/__manifest__.py
+++ pyper_website_snippets/__manifest__.py
@@ -22,12 +22,22 @@
         'views/snippets/snippets.xml',
 
         # Top Blocks
+        'views/snippets/top_blocks/s_top_banner_1.xml',
+        'views/snippets/top_blocks/s_top_banner_2.xml',
         'views/snippets/top_blocks/s_top_app_banner.xml',
         'views/snippets/top_blocks/s_top_banner_overflow_image_variations.xml',
         'views/snippets/top_blocks/s_top_banner_title_with_foreground_image.xml',
         'views/snippets/top_blocks/s_heading_masonry.xml',
         
         # Modern Blocks
+        'views/snippets/modern_blocks/s_video_banner.xml',
+        'views/snippets/modern_blocks/s_four_columns_title_text.xml',
+        'views/snippets/modern_blocks/s_two_columns_image_overflow.xml',
+        'views/snippets/modern_blocks/s_two_columns_image_overflow_right.xml',
+        'views/snippets/modern_blocks/s_grid_block_title_text.xml',
+        'views/snippets/modern_blocks/s_containerized_kpis.xml',
+        'views/snippets/modern_blocks/s_floating_blocks_with_text.xml',
+        'views/snippets/modern_blocks/s_title_and_values.xml',
         'views/snippets/modern_blocks/s_containerized_title_block.xml',
         'views/snippets/modern_blocks/s_row_banner_with_kpis.xml',
         'views/snippets/modern_blocks/s_faq_with_title.xml',
@@ -40,9 +50,6 @@
         'views/snippets/modern_blocks/s_feature_grid_with_links.xml',
         'views/snippets/modern_blocks/s_three_column_article.xml',
         'views/snippets/modern_blocks/s_section_with_card_link_items.xml',
-
-        # Modern Blocks old section : need RFC soon
-        'views/snippets/modern_blocks/snippets.xml',
 
     ],
     'assets': {
