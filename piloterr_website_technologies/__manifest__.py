6478a1bf1b4036e707b5d4c8e458a0339728d6ef
Fabien Maquin <fabien@krafter.io>
2024-09-27 08:59:08 +0200

    [ADD] Add missing summary in addon

--- piloterr_website_technologies/__manifest__.py
+++ piloterr_website_technologies/__manifest__.py
@@ -6,6 +6,7 @@
     'category': 'Sales/CRM',
     'license': 'Other proprietary',
     'description': 'Added the ability to identify the technologies used by a website from piloterr',
+    'summary': 'Added the ability to identify the technologies used by a website from piloterr',
     'version': '1.0',
     'author': 'Krafter SAS',
     'maintainer': [
