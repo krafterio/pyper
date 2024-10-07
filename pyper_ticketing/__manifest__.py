96b985cf01cd70a33db2f0dbe23cbe417f12dd13
gclement42 <clement@krafter.io>
2024-10-07 15:11:18 +0200

    [ADD] init addon files #PYP174

---
+++ pyper_ticketing/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Ticketing',
+    'category': 'Tools',
+    'license': 'Other proprietary',
+    'description': 'Ticketing system.',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'application': True,
+    'installable': True,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ticket_views.xml',
+        'views/menu.xml',
+    ],
+}
