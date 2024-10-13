f3e1e6f0c1e296f11c19969b71ab3ae22ff29aea
François Pluchino <francois@krafter.io>
2024-10-14 00:51:01 +0200

    [ADD] Add pyper_web_view to save and manage custom views

---
+++ pyper_web_view/static/src/search/control_panel/views_switcher.js
@@ -0,0 +1,320 @@
+/** @odoo-module */
+
+import {Component, useState} from '@odoo/owl';
+import {_t} from '@web/core/l10n/translation';
+import {x2ManyCommands} from '@web/core/orm_service';
+import {useService} from '@web/core/utils/hooks';
+import {Dropdown} from '@web/core/dropdown/dropdown';
+import {DropdownItem} from '@web/core/dropdown/dropdown_item';
+import {useOpenMany2XRecord, useSelectCreate} from '@web/views/fields/relational_utils';
+import {ConfirmationDialog} from '@web/core/confirmation_dialog/confirmation_dialog';
+
+export class ViewsSwitcher extends Component {
+    static template = 'pyper_web_view.ViewsSwitcher';
+
+    static props = {};
+
+    static components = {
+        Dropdown,
+        DropdownItem,
+    };
+
+    setup() {
+        this.actionService = useService('action');
+        this.dialogService = useService('dialog');
+        this.orm = useService('orm');
+
+        this.state = useState({
+            views: [],
+            selectedView: null,
+        });
+
+        const resModel = 'ir.views';
+
+        this.openMany2X = useOpenMany2XRecord({
+            resModel,
+            activeActions: {
+                create: true,
+                write: true,
+            },
+            isToMany: false,
+            fieldString: _t('Saved views'),
+            onRecordSaved: (record) => {
+                const view = {...record.data, id: record.resId};
+                const vIdx = this.state.views.findIndex(v => v.id === view.id);
+
+                if (vIdx !== -1) {
+                    this.state.views.splice(vIdx, 1, view);
+                }
+
+                this.selectView(view);
+            },
+            onRecordDiscarded: () => {},
+            onClose: () => {},
+        });
+
+        this.selectCreate = useSelectCreate({
+            resModel,
+            activeActions: {
+                write: true,
+            },
+            onSelected: (resId) => {
+                const resIds = Array.isArray(resId) ? resId : [resId];
+
+                if (resIds.length > 0) {
+                    const vIdx = this.state.views.findIndex(v => v.id === resIds[0]);
+
+                    if (vIdx !== -1) {
+                        this.selectView(this.state.views[vIdx]);
+                    }
+                }
+            },
+        });
+    }
+
+    get currentViewType() {
+        return this.env.config?.viewType || null;
+    }
+
+    get currentModelName() {
+        return this.env.model?.config?.resModel || this.env.searchModel?.resModel || null;
+    }
+
+    get currentLimit() {
+        return this.env.model?.config?.limit || this.actionService?.currentController?.action?.limit || 80;
+    }
+
+    get currentDomain() {
+        return this.env.model?.config?.domain || this.env.searchModel?.domain || [];
+    }
+
+    get currentOrderBy() {
+        return this.env.model?.config?.orderBy || this.env.searchModel?.orderBy || [];
+    }
+
+    get currentGroupBy() {
+        return this.env.model?.config?.groupBy || this.env.searchModel?.groupBy || [];
+    }
+
+    get displaySwitcher() {
+        return !this.excludedViewTypes.includes(this.currentViewType)
+            && ![undefined, 'ir.views'].includes(this.currentModelName);
+    }
+
+    get excludedViewTypes() {
+        return ['form'];
+    }
+
+    get viewsFields() {
+        return ['id', 'name', 'category', 'shared', 'view_mode', 'ir_action_id'];
+    }
+
+    get domain() {
+        const categories = this.availableCategories.map(item => item[0]);
+
+        return [['res_model_id.model', '=', this.currentModelName], ['category', 'in', categories]];
+    }
+
+    get limit() {
+        return 300;
+    }
+
+    get viewModeIcons() {
+        return {
+            'tree': 'oi oi-view-list',
+            'kanban': 'oi oi-view-kanban',
+            undefined: 'fa fa-columns',
+        }
+    }
+
+    get togglerIcon() {
+        return this.getViewIcon(this.state.selectedView?.view_mode);
+    }
+
+    get togglerLabel() {
+        return this.state.selectedView?.name || this.placeholder;
+    }
+
+    get placeholder() {
+        return _t('Select a view');
+    }
+
+    get createLabel() {
+        return _t('Create a new view');
+    }
+
+    get availableCategories() {
+        return [
+            ['system', null],
+            ['personal', _t('My views')],
+            ['shared', _t('Shared views')],
+        ];
+    }
+
+    get availableViewTypes() {
+        return {
+            'tree': 'tree',
+            'list': 'tree',
+            'kanban': 'kanban',
+            undefined: 'tree',
+        }
+    }
+
+    get categorizedViews() {
+        const categoryViews = [];
+        const categ = {};
+        this.availableCategories.forEach((cat) => categ[cat[0]] = []);
+
+        this.state.views.forEach((view) => {
+            categ[view.category].push(view);
+        });
+
+        this.availableCategories.forEach((cat) => {
+            categoryViews.push({
+                value: cat[0],
+                name: cat[1],
+                views: categ[cat[0]] ? [...categ[cat[0]]] : [],
+            });
+        });
+
+        return categoryViews;
+    }
+
+    getViewIcon(viewMode) {
+        return this.viewModeIcons[viewMode] || this.viewModeIcons[undefined];
+    }
+
+    getValidViewType(viewMode) {
+        return this.availableViewTypes[viewMode] || this.availableViewTypes[undefined];
+    }
+
+    async onDropdownOpen() {
+        if (this.state.views.length > 0) {
+            this.state.views.splice(0, this.state.views.length);
+        }
+
+        await this.loadViews();
+    }
+
+    selectView(view) {
+        this.state.selectedView = view || null;
+
+        if (view && view['ir_action_id'] && view['ir_action_id'][0]) {
+            this.actionService.doAction(view['ir_action_id'][0]).then();
+        }
+    }
+
+    configureViews(categoryName) {
+        this.selectCreate({
+            domain: [['category', '=', categoryName], ...this.domain],
+            context: this.getContext(),
+            title: _t('Configure views'),
+        });
+    }
+
+    async createView() {
+        await this.openMany2X({context: this.getContext()});
+    }
+
+    async editView(viewId) {
+        await this.openMany2X({resId: viewId});
+    }
+
+    openConfirmationDialog(viewId) {
+        const {shared} = this.state.views.find((view) => view.id === viewId);
+        const dialogProps = {
+            title: _t('Warning'),
+            body: shared
+                ? _t('This view is shared and will be removed for everyone.')
+                : _t('Are you sure that you want to remove this view?'),
+            confirmLabel: _t('Delete view'),
+            confirm: async () => {
+                await this.orm.unlink('ir.views', [viewId]);
+
+                if (this.state.selectedView?.id === viewId) {
+                    this.selectView(null);
+                }
+
+                const vIdx = this.state.views.findIndex(v => v.id === viewId);
+
+                if (vIdx !== -1) {
+                    this.state.views.splice(vIdx, 1);
+                }
+            },
+            cancel: () => {},
+        };
+        this.dialogService.add(ConfirmationDialog, dialogProps);
+    }
+
+    async loadViews() {
+        this.state.views = await this.orm.searchRead(
+            'ir.views',
+            this.domain,
+            this.viewsFields,
+            {
+                limit: this.limit,
+            }
+        );
+    }
+
+    getContext() {
+        const context = {};
+        context['default_res_model_name'] = this.currentModelName;
+        context['default_view_mode'] = this.getValidViewType(this.currentViewType);
+        context['default_limit'] = this.currentLimit;
+        context['default_domain'] = this.currentDomain;
+        context['default_res_field_ids'] = [];
+        context['default_res_group_by_ids'] = [];
+        context['default_res_order_by_ids'] = [];
+
+        if (this.currentGroupBy.length > 0) {
+            const items = [];
+
+            this.currentGroupBy.forEach((groupName) => {
+                context['default_res_group_by_ids'].push(x2ManyCommands.create(undefined, {
+                    'field_name': groupName,
+                }));
+            });
+        }
+
+        if (this.currentOrderBy.length > 0) {
+            const item = this.currentOrderBy[0];
+            context['default_res_order_by_ids'].push(x2ManyCommands.create(undefined, {
+                'field_name': item.name,
+                'sort': item.asc ? 'asc': 'desc',
+            }));
+        }
+
+        if (['tree', 'list'].includes(this.currentViewType)) {
+            const fields = this.env.config.viewArch.querySelectorAll('field');
+
+            fields.forEach(field => {
+                const vals = {
+                    'field_name': field.getAttribute('name'),
+                };
+                const string = field.getAttribute('string');
+                const invisible = field.getAttribute('column_invisible');
+                const optional = field.getAttribute('optional');
+                const widget = field.getAttribute('widget');
+
+                if (string) {
+                    vals['label'] = string;
+                }
+
+                if (invisible) {
+                    vals['optional'] = 'invisible';
+                } else if (optional) {
+                    vals['optional'] = optional;
+                }
+
+                if (widget) {
+                    vals['widget'] = widget;
+                }
+
+                context['default_res_field_ids'].push(x2ManyCommands.create(undefined, vals));
+            });
+        }
+
+        return context;
+    }
+}
