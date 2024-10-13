a3cb15a94028524a0bc6b41e5f7def45f34a4094
Fabien Maquin <fabien@krafter.io>
2024-10-13 22:30:56 +0200

    [ADD] Add Pyper Open AI connector addon

---
+++ pyper_openai_connector/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Open AI Connector',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'Add ability to use Open AI API',
+    'summary': 'Add ability to use Open AI API',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'application': True,
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'website': 'https://krafter.io',
+    'depends': [
+        'base',
+        'web',
+    ],
+    'data': [
+        # Views
+        'views/res_config_settings_views.xml',
+    ],
+    'assets': {
+    },
+}
