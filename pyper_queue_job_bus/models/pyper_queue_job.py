5e6aa610c8db992ac7dbcd66d1234368b17e8a93
François Pluchino <francois@krafter.io>
2024-10-27 12:31:16 +0100

    [IMP] Add default message when user is notified for a delayed action

--- pyper_queue_job_bus/models/pyper_queue_job.py
+++ pyper_queue_job_bus/models/pyper_queue_job.py
@@ -7,12 +7,12 @@ from odoo import _, models
 class PyperQueueJob(models.Model):
     _inherit = 'pyper.queue.job'
 
-    def notify(self, message: str, title: str = None, notif_type: str = 'info'):
+    def notify(self, message: str = None, title: str = None, notif_type: str = 'info'):
         self.ensure_one()
         self.env['bus.bus']._sendone(self.env.user.partner_id, 'simple_notification', {
             'type': notif_type,
             'title': title if title else _('Delayed action'),
-            'message': message,
+            'message': message if message else _('The action has been queued'),
         })
 
         return self
