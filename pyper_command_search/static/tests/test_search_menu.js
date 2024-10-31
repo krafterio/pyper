ced14cf483843a97a44296b5a21cb1d91202a15d
Jeremy Braconnier <jeremy@krafter.io>
2024-10-31 14:26:37 +0100

    [ADD] Unit test for pyper_command_search #PYP234
    Test if button opens the command palette.

--- pyper_command_search/static/tests/test_search_menu.js
+++ pyper_command_search/static/tests/test_search_menu.js
@@ -18,11 +18,17 @@ QUnit.module("PyperCommandSearch", {
         // Register the SearchMenu component in the systray
         systrayRegistry.add("pyper_command_search.search_menu", { Component: SearchMenu });
 
-        // Register services
-        serviceRegistry.add("command", {
-            openMainPalette: () => {},
+        // Mock command service
+        const commandServiceMock = {
+            openMainPalette() {
+                commandServiceMock.commandPaletteOpened = true;
+            },
             start: () => {},
-        });
+            commandPaletteOpened: false,
+        };
+        
+        // Register the mocked service
+        serviceRegistry.add("command", commandServiceMock);
     },
 });
 
@@ -34,5 +40,24 @@ QUnit.test("TestSearchSystrayExists", async (assert) => {
     assert.ok(
         searchMenuElement,
         "Search Menu is rendered with the class .o-command-search-SearchSystray-class"
+    );
+});
+
+QUnit.test("TestOpenSystray", async (assert) => {
+    let env = await makeTestEnv({});
+
+    const commandService = serviceRegistry.get("command");
+    // Pass the mocked command service to the searchMenu
+    env.services.command = commandService;
+
+    await mount(SearchMenu, target, { env });
+
+    const button = target.querySelector(".dropdown-toggle");
+    commandService.commandPaletteOpened = false;
+
+    button.click();
+    assert.ok(
+        commandService.commandPaletteOpened,
+        "Command palette should open when the button is clicked"
     );
 });
