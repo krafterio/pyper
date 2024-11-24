07f4e489ec6c0ed4570da86d61c2ce79bb59759d
François Pluchino <francois@krafter.io>
2024-11-24 14:26:05 +0100

    [IMP] Always use recaptcha even if javascript is disabled in browser

--- pyper_recaptcha_login/controllers/home.py
+++ pyper_recaptcha_login/controllers/home.py
@@ -14,24 +14,28 @@ logger = logging.getLogger(__name__)
 class Home(BaseHome):
     @http.route()
     def web_login(self, redirect=None, **kw):
-        recaptcha_token = kw.pop('g-recaptcha-response', False)
+        ensure_db()
+        param = request.env['ir.config_parameter'].sudo().get_param
 
-        if recaptcha_token:
-            try:
-                ensure_db()
-                param = request.env['ir.config_parameter'].sudo().get_param
-                if param('pyper_recaptcha_login.enabled') and request.httprequest.method == 'POST':
-                    # Replace 'g-recaptcha-response' input by 'recaptcha_token_response' request params
-                    request.params.update({'recaptcha_token_response': recaptcha_token})
+        if 'recaptcha_login' not in request.params:
+            request.params.update({'recaptcha_login': param('pyper_recaptcha_login.enabled', False)})
 
-                    if not request.env['ir.http']._verify_request_recaptcha_token('login_form'):
-                        raise UserError(_('Invalid recaptcha'))
-            except (ValidationError, UserError) as e:
-                request.params['login_success'] = False
-                values = request.params.copy()
-                values.update({'error': str(e)})
+        if not request.params.get('recaptcha_login'):
+            return super().web_login(redirect=redirect, **kw)
 
-                return request.render('web.login', values)
+        try:
+            if request.httprequest.method == 'POST':
+                # Replace 'g-recaptcha-response' input by 'recaptcha_token_response' request params
+                request.params.update({'recaptcha_token_response': kw.pop('g-recaptcha-response', False)})
+
+                if not request.env['ir.http']._verify_request_recaptcha_token('login_form'):
+                    raise UserError(_('Invalid recaptcha'))
+        except (ValidationError, UserError) as e:
+            request.params['login_success'] = False
+            values = request.params.copy()
+            values.update({'error': str(e)})
+
+            return request.render('web.login', values)
 
         response = super().web_login(redirect=redirect, **kw)
 
--- pyper_recaptcha_signup/controllers/signup.py
+++ pyper_recaptcha_signup/controllers/signup.py
@@ -14,28 +14,35 @@ logger = logging.getLogger(__name__)
 class AuthSignupHome(BaseAuthSignupHome):
     @http.route()
     def web_auth_signup(self, redirect=None, **kw):
-        recaptcha_token = kw.pop('g-recaptcha-response', False)
+        ensure_db()
+        param = request.env['ir.config_parameter'].sudo().get_param
 
-        if recaptcha_token:
-            try:
-                ensure_db()
-                param = request.env['ir.config_parameter'].sudo().get_param
-                if param('pyper_recaptcha_signup.enabled') and request.httprequest.method == 'POST':
-                    # Replace 'g-recaptcha-response' input by 'recaptcha_token_response' request params
-                    request.params.update({'recaptcha_token_response': recaptcha_token})
+        if 'recaptcha_signup' not in request.params:
+            request.params.update({
+                'recaptcha_signup': param('pyper_recaptcha_signup.enabled', False),
+                'recaptcha_login': False, # Disable recaptcha on login in same request if it enabled
+            })
 
-                    if not request.env['ir.http']._verify_request_recaptcha_token('signup_form'):
-                        raise UserError(_('Invalid recaptcha'))
-            except (ValidationError, UserError) as e:
-                request.params['signup_success'] = False
-                values = request.params.copy()
-                values.update({'error': str(e)})
+        if not request.params.get('recaptcha_signup'):
+            return super().web_auth_signup(redirect=redirect, **kw)
 
-                return request.render('auth_signup.signup', values)
+        try:
+            if request.httprequest.method == 'POST':
+                # Replace 'g-recaptcha-response' input by 'recaptcha_token_response' request params
+                request.params.update({'recaptcha_token_response': kw.pop('g-recaptcha-response', False)})
+
+                if not request.env['ir.http']._verify_request_recaptcha_token('signup_form'):
+                    raise UserError(_('Invalid recaptcha'))
+        except (ValidationError, UserError) as e:
+            request.params['signup_success'] = False
+            values = request.params.copy()
+            values.update({'error': str(e)})
+
+            return request.render('auth_signup.signup', values)
 
         response = super().web_auth_signup(redirect=redirect, **kw)
 
-        if not request.params.get('signup_success', False) and 'Content-Security-Policy' in request.params:
+        if not request.params.get('signup_success', False) and 'Content-Security-Policy' in response.headers:
             response.headers.pop('Content-Security-Policy')
 
         return response
