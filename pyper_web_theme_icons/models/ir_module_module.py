d527682b46fc7cd3dfc193aae3e7ffdbf5bd7144
François Pluchino <francois@krafter.io>
2024-11-26 20:55:01 +0100

    [IMP] Improve replacement of menu item icons

--- pyper_web_theme_icons/models/ir_module_module.py
+++ pyper_web_theme_icons/models/ir_module_module.py
@@ -2,19 +2,13 @@
 # Krafter Proprietary License (see LICENSE file).
 
 from odoo import models
-from odoo.addons.base.models.ir_module import assert_log_admin_access
 
 
 class IrModuleModule(models.Model):
     _inherit = 'ir.module.module'
 
-    @assert_log_admin_access
-    def button_install(self):
-        res = super().button_install()
+    def _button_immediate_function(self, function):
+        res = super()._button_immediate_function(function)
         self.env['ir.ui.menu'].sudo()._update_root_icons()
 
         return res
-
-    def button_upgrade(self):
-        super().button_upgrade()
-        self.env['ir.ui.menu'].sudo()._update_root_icons()
