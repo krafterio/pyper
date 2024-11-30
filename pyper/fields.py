0986e14dd68f6216eebb63a3dd90437e2ae4ed6b
François Pluchino <francois@krafter.io>
2024-11-30 22:38:23 +0100

    [ADD] Add selection_replace option in fields.Selection

--- pyper/fields.py
+++ pyper/fields.py
@@ -1,9 +1,14 @@
 # Copyright Krafter SAS <hey@krafter.io>
 # Krafter Proprietary License (see LICENSE file).
 
+import logging
 import odoo
 
+from odoo.fields import resolve_mro
 from xmlrpc.client import MAXINT
+
+
+_logger = logging.getLogger(__name__)
 
 
 class IntegerFalsable(odoo.fields.Field):
@@ -53,3 +58,62 @@ class IntegerFalsable(odoo.fields.Field):
 
 
 odoo.fields.IntegerFalsable = IntegerFalsable
+
+# Extend Selection field
+_original_field_selection_get_attrs = odoo.fields.Selection._get_attrs
+def _field_selection_get_attrs(self, model_class, name):
+    attrs = _original_field_selection_get_attrs(self, model_class, name)
+    attrs.pop('selection_replace', None)
+
+    return attrs
+
+
+_original_field_selection_setup_attrs = odoo.fields.Selection._setup_attrs
+def _field_selection_setup_attrs(self, model_class, name):
+    _original_field_selection_setup_attrs(self, model_class, name)
+
+    for field in self._base_fields:
+        if 'selection_replace' in field.args:
+            if self.related:
+                _logger.warning("%s: selection_replace attribute will be ignored as the field is related", self)
+
+            editable_selection = {t[0]: t[1] for t in self.selection}
+            selection_replace = field.args['selection_replace']
+
+            assert isinstance(selection_replace, list), \
+                "%s: selection_replace=%r must be a list" % (self, selection_replace)
+
+            for selection in selection_replace:
+                assert selection[0] in editable_selection, \
+                    "%s: selection_replace=%r must existing" % (self, selection_replace)
+
+                editable_selection[selection[0]] = selection[1]
+
+            new_selection = list(editable_selection.items())
+            self.selection = new_selection
+
+
+_original_field_selection_selection_modules = odoo.fields.Selection._selection_modules
+def _field_selection_selection_modules(self, model):
+    value_modules = _original_field_selection_selection_modules(self, model)
+
+    if not isinstance(self.selection, list):
+        return value_modules
+
+    for field in reversed(resolve_mro(model, self.name, type(self).__instancecheck__)):
+        module = field._module
+
+        if not module:
+            continue
+
+        if 'selection_replace' in field.args:
+            for value_label in field.args['selection_replace']:
+                if len(value_label) > 1:
+                    value_modules[value_label[0]].add(module)
+
+    return value_modules
+
+
+odoo.fields.Selection._get_attrs = _field_selection_get_attrs
+odoo.fields.Selection._setup_attrs = _field_selection_setup_attrs
+odoo.fields.Selection._selection_modules = _field_selection_selection_modules
