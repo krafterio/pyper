675a07dd7985f675ed45390e367af7646398fd5f
François Pluchino <francois@krafter.io>
2024-12-01 15:05:36 +0100

    [ADD] Add web theme control panel addon

---
+++ pyper_web_theme_control_panel/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+{
+    'name': 'Web Theme Control Panel',
+    'category': 'Hidden/Tools',
+    'license': 'Other proprietary',
+    'description': 'Make compatible Control Panel with Pyper Web Theme',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'website': 'https://krafter.io',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'auto_install': [
+        'pyper_web_theme',
+    ],
+    'depends': [
+        'base',
+        'web',
+        'pyper_control_panel',
+        'pyper_web_theme',
+    ],
+}
