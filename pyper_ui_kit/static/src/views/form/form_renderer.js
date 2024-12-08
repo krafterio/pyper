979298fe7b32bbc6646d667872463bd97ffc1398
François Pluchino <francois@krafter.io>
2024-12-08 10:01:22 +0100

    [FIX] Inject new components with patch in existing component
    And allow to use all injected components in other component extending the current component

--- pyper_ui_kit/static/src/views/form/form_renderer.js
+++ pyper_ui_kit/static/src/views/form/form_renderer.js
@@ -2,6 +2,12 @@
 
 import {FormRenderer} from '@web/views/form/form_renderer';
 import {ButtonActions} from './button_actions';
+import {patch} from '@web/core/utils/patch';
 
-FormRenderer.components.ButtonActions = ButtonActions;
-FormRenderer.components.SheetButtonBox = FormRenderer.components.ButtonBox;
+patch(FormRenderer.prototype, {
+    setup() {
+        this.constructor.components.ButtonActions = ButtonActions;
+        this.constructor.components.SheetButtonBox = FormRenderer.components.ButtonBox;
+        super.setup();
+    },
+});
