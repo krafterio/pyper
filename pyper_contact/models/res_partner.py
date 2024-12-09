b29d23b029f64bbd1b279e8aaccc4c14659602df
Loup Martineau <loup@krafter.io>
2024-12-09 16:20:06 +0100

    [ADD] Display 10 digit phone number #PYP286

--- pyper_contact/models/res_partner.py
+++ pyper_contact/models/res_partner.py
@@ -18,3 +18,29 @@ class ResPartner(models.Model):
         for partner in self:
             partner.is_user = len(partner.user_ids) > 0
 
+
+    @api.onchange('phone', 'country_id', 'company_id')
+    def _onchange_phone_validation(self):
+        for contact in self:
+            if contact.phone:
+                if len(contact.phone) == 10 and contact.phone.isdigit():
+                    contact.phone = contact._display_a_ten_digit_phone(contact.phone)
+                else:
+                    super(ResPartner, self)._onchange_phone_validation() 
+
+
+    @api.onchange('mobile', 'country_id', 'company_id')
+    def _onchange_mobile_validation(self):
+        for contact in self:
+            if contact.mobile:
+                if len(contact.mobile) == 10 and contact.mobile.isdigit():
+                    contact.mobile = contact._display_a_ten_digit_phone(contact.mobile)
+                else:
+                    super(ResPartner, self)._onchange_mobile_validation() 
+
+
+    def _display_a_ten_digit_phone(self, phone):
+        return f'{phone[:2]} {phone[2:4]} {phone[4:6]} {phone[6:8]} {phone[8:]}'
+
+
+    
