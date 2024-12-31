5ea23c9ae0b3e55921228bc810bb25ddcbec3789
François Pluchino <francois@krafter.io>
2024-12-31 18:02:31 +0100

    [ADD] Keep field values of mail activities when activities are done in mail messages

---
+++ pyper_mail/models/mail_activity.py
@@ -0,0 +1,27 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+from odoo import models
+
+from markupsafe import Markup
+
+
+class MailActivity(models.Model):
+    _inherit = 'mail.activity'
+
+    def _action_done(self, feedback=False, attachment_ids=None):
+        res = super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
+
+        for model, activity_data in self._classify_by_model().items():
+            for message, activity in zip(res[0], activity_data['activities']):
+                message.subject = activity.summary
+                message.description = False
+                message.body = activity.note
+
+                if feedback:
+                    if message.body:
+                        message.body += Markup('<br>') + feedback
+                    else:
+                        message.body = feedback
+
+        return res
