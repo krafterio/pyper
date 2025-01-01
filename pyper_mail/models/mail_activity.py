7726707f631c8e4b85d016715bcee1507bf380cc
François Pluchino <francois@krafter.io>
2025-01-01 14:32:31 +0100

    [IMP] Keep assigned user when activity is done

--- pyper_mail/models/mail_activity.py
+++ pyper_mail/models/mail_activity.py
@@ -15,7 +15,7 @@ class MailActivity(models.Model):
         for model, activity_data in self._classify_by_model().items():
             for message, activity in zip(res[0], activity_data['activities']):
                 message.subject = activity.summary
-                message.description = False
+                message.author_id = activity.user_id.partner_id
                 message.body = activity.note
 
                 if feedback:
