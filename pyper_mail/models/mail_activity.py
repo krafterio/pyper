e9d8e5096f78fa409e77a4d53232afdbf1175e2f
François Pluchino <francois@krafter.io>
2025-01-02 17:10:56 +0100

    [FIX] Fix deleted activity when activity is done and converted to message

--- pyper_mail/models/mail_activity.py
+++ pyper_mail/models/mail_activity.py
@@ -3,23 +3,31 @@
 
 from odoo import models
 
-from markupsafe import Markup
-
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
 
     def _action_done(self, feedback=False, attachment_ids=None):
-        res = super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
+        vals_activities = []
 
         for model, activity_data in self._classify_by_model().items():
-            for message, activity in zip(res[0], activity_data['activities']):
-                message.subject = activity.summary
-                message.author_id = activity.user_id.partner_id
-                message.body = activity.note
-                message.feedback = feedback
+            for activity in activity_data['activities']:
+                vals_activities.append({
+                    'id': activity.id,
+                    'subject': activity.summary,
+                    'author_id': activity.user_id.partner_id,
+                    'body': activity.note,
+                })
 
-                if message.mail_activity_type_id and message.mail_activity_type_id.id == self.env.ref('mail.mail_activity_data_email').id:
-                    message.message_type = 'email_outgoing'
+        res = super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
+
+        for message, activity in zip(res[0], vals_activities):
+            message.subject = activity.get('subject', False)
+            message.author_id = activity.get('author_id', False)
+            message.body = activity.get('body', False)
+            message.feedback = feedback
+
+            if message.mail_activity_type_id and message.mail_activity_type_id.id == self.env.ref('mail.mail_activity_data_email').id:
+                message.message_type = 'email_outgoing'
 
         return res
