4be062fb1a502bbc45bf1e53c09fbbde2bf7df41
Guillaume Simonet <guillaume@krafter.io>
2025-01-22 10:36:13 +0100

    [RMV] Remove model connectivity

--- pyper_product_extend_features_it/models/__init__.py
+++ pyper_product_extend_features_it/models/__init__.py
@@ -12,7 +12,6 @@ from . import product_processor
 from . import product_screen_size
 from . import product_graphic_card
 from . import product_screen_quality
-from . import product_connectivity
 from . import product_video_output
 from . import product_video_input
 from . import product_network
