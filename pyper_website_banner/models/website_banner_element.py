b738e534554884eddb287f742da6c4f7651c4a81
Guillaume Simonet <guillaume@krafter.io>
2025-02-07 10:46:02 +0100

    [ADD] Add type for banner's element, and add dynamism on the view in backoffice

--- pyper_website_banner/models/website_banner_element.py
+++ pyper_website_banner/models/website_banner_element.py
@@ -1,8 +1,7 @@
 # Copyright Krafter SAS <hey@krafter.io>
 # Krafter Proprietary License (see LICENSE file).
 
-from odoo import _, fields, models
-from datetime import datetime
+from odoo import api, fields, models
 
 
 class WebsiteBannerElement(models.Model):
@@ -10,18 +9,32 @@ class WebsiteBannerElement(models.Model):
     _description = 'Banner header\'s element on website'
     
     sequence = fields.Integer()
+    
+    type = fields.Selection([
+        ('text', 'Text'),
+        ('link', 'Link'),
+        ('product', 'Product'),
+        ('page', 'Page'),
+    ])
 
     content = fields.Char()
-    text_color = fields.Char()
-
-    link = fields.Char()
     
     is_btn = fields.Boolean('Button style')
-    bg_color = fields.Char('Background color')
-    outline_color = fields.Char('Border color')
-    
+    text_color = fields.Char('Text')
+    bg_color = fields.Char('Background')
+    outline_color = fields.Char('Border')
     is_bold = fields.Boolean('Bold')
     is_italic = fields.Boolean('Italic')
 
+    link = fields.Char()
     product_tmpl_id = fields.Many2one('product.template', 'Product')
     page_id = fields.Many2one('website.page', 'Page')
+    
+    def action_remove_color_bg(self):
+        self.bg_color = False
+        
+    def action_remove_color_text(self):
+        self.text_color = False
+        
+    def action_remove_color_outline(self):
+        self.outline_color = False
