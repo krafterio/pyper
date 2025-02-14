4a22e14244af05cb9572eff4fb2ab6a111aba5bb
François Pluchino <francois@krafter.io>
2025-02-14 10:32:04 +0100

    [ADD] Add main partner in sale order #PYP337

--- pyper_sale_extra/models/sale_order.py
+++ pyper_sale_extra/models/sale_order.py
@@ -35,6 +35,24 @@ class SaleOrder(models.Model):
         compute='_compute_display_signature_information_on_document',
     )
 
+    main_partner_id = fields.Many2one(
+        'res.partner',
+        string='Main Customer',
+        compute='_compute_main_partner_id',
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends('partner_id')
+    def _compute_main_partner_id(self):
+        for move in self:
+            partner = move.partner_id
+
+            while partner and partner.parent_id:
+                partner = partner.parent_id
+
+            move.main_partner_id = partner
+
     @api.depends('payment_method_id')
     def _compute_display_bank_account_on_document(self):
         for order in self:
