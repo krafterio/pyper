d14631461d4b7228263749ff61c73b99d0db411d
Clément Gerardin <clement@krafter.io>
2025-02-19 10:48:54 +0100

    [ADD] Add action for edit done activity #PYP333

--- pyper_mail/models/mail_thread.py
+++ pyper_mail/models/mail_thread.py
@@ -17,3 +17,18 @@ class MailThread(models.AbstractModel):
             return _('The company %s was created', self[self._rec_name])
 
         return super()._creation_message()
+
+    def _check_can_update_message_content(self, messages):
+        messages_notification_ids = []
+        for message in messages:
+            if message.message_type == 'notification':
+                messages_notification_ids.append(message.id)
+                message.message_type = 'comment'
+        res = super()._check_can_update_message_content(messages)
+        for message in messages:
+            if message.id in messages_notification_ids:
+                message.message_type = 'notification'
+        return res
+
+
+
