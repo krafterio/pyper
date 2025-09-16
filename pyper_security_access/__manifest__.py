307a4d28f57872ba19df0f2dc623d76b1cc8e005
François Pluchino <francois@krafter.io>
2025-09-16 16:45:11 +0200

    [ADD] Make compatible pyper_security_access with Odoo 18.0

--- pyper_security_access/__manifest__.py
+++ pyper_security_access/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Technical',
     'license': 'LGPL-3',
     'description': 'Allow to add security access management on all models.',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
