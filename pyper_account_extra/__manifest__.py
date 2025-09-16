db80949e92811af924e492da048c6fbafaadd2e7
François Pluchino <francois@krafter.io>
2025-09-16 20:58:32 +0200

    [ADD] Make compatible pyper_account_extra with Odoo 18.0

--- pyper_account_extra/__manifest__.py
+++ pyper_account_extra/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Accounting/Payment',
     'license': 'LGPL-3',
     'description': 'Extend Account features.',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
