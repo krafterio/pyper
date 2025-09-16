79e2409d2d08e46981e115ddd59812f8ba48644a
François Pluchino <francois@krafter.io>
2025-09-16 15:44:18 +0200

    [ADD] Make compatible pyper_application with Odoo 18.0

--- pyper_application/__manifest__.py
+++ pyper_application/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Technical',
     'license': 'LGPL-3',
     'description': 'Base of API to connect an external application.',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
