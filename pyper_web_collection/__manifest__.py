596c2713823c1d1b571a2440518280aa2f28c2fd
François Pluchino <francois@krafter.io>
2025-09-16 19:08:13 +0200

    [ADD] Make compatible pyper_web_collection with Odoo 18.0

--- pyper_web_collection/__manifest__.py
+++ pyper_web_collection/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Hidden/Tools',
     'license': 'LGPL-3',
     'description': 'Link records in collections',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'website': 'https://krafter.io',
     'maintainer': [
