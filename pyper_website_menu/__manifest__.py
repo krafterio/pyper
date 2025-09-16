3ebf433ddef54ef4f61ef70358e877a11b8e905d
François Pluchino <francois@krafter.io>
2025-09-16 21:25:58 +0200

    [ADD] Make compatible pyper_website_menu with Odoo 18.0

--- pyper_website_menu/__manifest__.py
+++ pyper_website_menu/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Hidden/Tools',
     'license': 'LGPL-3',
     'description': 'Use menu items to build structured mega menu for website',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'installable': True,
     'application': False,
