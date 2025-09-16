655c115bb26a564580767ab5263736babf0dcefe
François Pluchino <francois@krafter.io>
2025-09-16 16:26:22 +0200

    [ADD] Make compatible pyper_map with Odoo 18.0

--- pyper_map/__manifest__.py
+++ pyper_map/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Tools',
     'license': 'LGPL-3',
     'description': 'Map component to create views with map',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
