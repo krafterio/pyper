d4a5bb0ea675ad2bb66efe34a1bd0629f865142a
François Pluchino <francois@krafter.io>
2025-09-16 21:24:38 +0200

    [ADD] Make compatible pyper_website_saas with Odoo 18.0

--- pyper_website_saas/__manifest__.py
+++ pyper_website_saas/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Hidden',
     'license': 'LGPL-3',
     'description': 'Website configuration for SaaS',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
