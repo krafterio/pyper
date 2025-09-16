eeb6110bdf093c9a1d60f8979512f27029090307
François Pluchino <francois@krafter.io>
2025-09-16 21:20:47 +0200

    [ADD] Make compatible pyper_mail_sync with Odoo 18.0

--- pyper_mail_sync/__manifest__.py
+++ pyper_mail_sync/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Hidden/Tools',
     'license': 'LGPL-3',
     'description': 'Sync all messages from IMAP account',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Krafter SAS',
     'maintainer': [
         'Krafter SAS',
