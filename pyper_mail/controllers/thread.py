363af73bd4eba3ac0225a2832a004e56138e5207
François Pluchino <francois@krafter.io>
2026-03-26 18:55:26 +0100

    [ADD] Make compatible pyper_mail with Odoo 19.0

--- pyper_mail/controllers/thread.py
+++ pyper_mail/controllers/thread.py
@@ -7,7 +7,7 @@ from odoo.addons.mail.controllers.thread import ThreadController as BaseThreadCo
 
 
 class ThreadController(BaseThreadController):
-    @http.route('/mail/thread/messages', methods=['POST'], type='json', auth='user')
+    @http.route('/mail/thread/messages', methods=['POST'], type='jsonrpc', auth='user')
     def mail_thread_messages(self, thread_model, thread_id, search_term=None, before=None, after=None, around=None, limit=30):
         if self._include_children_messages(thread_model):
             request.update_context(mail_message_with_children_res_ids=request.env[thread_model].search([('parent_id', '=', thread_id)]).ids)
