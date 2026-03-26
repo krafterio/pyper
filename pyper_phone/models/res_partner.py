1d0f167c4cddd033a65db1d6987e3cdc878e6d2e
François Pluchino <francois@krafter.io>
2026-03-26 18:41:40 +0100

    [ADD] Make compatible pyper_phone with Odoo 19.0

--- pyper_phone/models/res_partner.py
+++ pyper_phone/models/res_partner.py
@@ -7,17 +7,9 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # Format phone and mobile fields separately because phone_sanitized use phone field or mobile field
     phone_formatted = fields.Char(
         'Phone formatted',
         compute='_compute_phone_formatted',
-        compute_sudo=True,
-        store=True,
-    )
-
-    mobile_formatted = fields.Char(
-        'Mobile formatted',
-        compute='_compute_mobile_formatted',
         compute_sudo=True,
         store=True,
     )
@@ -26,8 +18,3 @@ class ResPartner(models.Model):
     def _compute_phone_formatted(self):
         for record in self:
             record.phone_formatted = record._phone_format(fname='phone') if record.phone else False
-
-    @api.depends('mobile')
-    def _compute_mobile_formatted(self):
-        for record in self:
-            record.mobile_formatted = record._phone_format(fname='mobile') if record.mobile else False
